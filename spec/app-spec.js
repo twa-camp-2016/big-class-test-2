fca8b3c0c6678600204a9ac1c636eed7bc102d4b
wanggenwang <18829273239@163.com>
2016-07-30 15:41:44 +0800

    getAmount

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -15,3 +15,21 @@ describe('formatTags',()=>{
         expect(obj.formatTags(tags)).toEqual(formattedTags);
     });
 });
+describe('getAmount',()=>{
+    it('return barcodesAmount',()=>{
+        let formattedTags=[
+            {barcode:'ITEM01', amount:1},
+            {barcode:'ITEM01', amount:1},
+            {barcode:'ITEM01', amount:1},
+            {barcode:'ITEM02', amount:2},
+            {barcode:'ITEM03', amount:2},
+            {barcode:'ITEM03', amount:1},
+        ];
+        let barcodesAmount=[
+            {barcode:'ITEM01', amount:3},
+            {barcode:'ITEM02', amount:2},
+            {barcode:'ITEM03', amount:3},
+        ];
+        expect(obj.getAmount(formattedTags)).toEqual(barcodesAmount);
+    });
+});
--- src/app.js
+++ src/app.js
@@ -7,6 +7,24 @@ function formatTags(tags) {
         };
     });
 }
+function getAmount(formattedTags) {
+    let barcodesAmount = [];
+    for (let i = 0; i < formattedTags.length; i++) {
+        let j;
+        for (j = 0; j < barcodesAmount.length; j++)
+            if (formattedTags[i].barcode === barcodesAmount[j].barcode)
+                break;
+        if (j === barcodesAmount.length) {
+            barcodesAmount[j] = {barcode: formattedTags[i].barcode, amount: formattedTags[i].amount};
+        }
+        else {
+            barcodesAmount[j].amount += formattedTags[i].amount;
+        }
+    }
+    return barcodesAmount;
+}
+
 module.exports = {
-    formatTags: formatTags
+    formatTags: formatTags,
+    getAmount: getAmount
 };
