4a8bec54ef4eb83cc20a897ff4238825b0671361
Miss-qi <1370322806@qq.com>
2016-07-30 11:29:16 +0800

    matchPromotions

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,42 +1,84 @@
 'use strict';
 const pos = require("../src/app");
+const load = require("./fixture");
 
-describe("separateTags",function () {
-    it("should separate tags",function () {
-        let tags = ['IEM0000000','IEM0000003-2'];
+describe("separateTags", function () {
+    it("should separate tags", function () {
+        let tags = ['IEM0000000', 'IEM0000003-2'];
         let result = pos.separateTags(tags);
         let itemsTag = [
             {
-                barcode:'IEM0000000',
-                count:1
+                barcode: 'IEM0000000',
+                count: 1
             },
             {
-                barcode:'IEM0000003',
-                count:2
+                barcode: 'IEM0000003',
+                count: 2
             }
         ];
         expect(result).toEqual(itemsTag);
     })
 });
 
-describe("amountBarcodes",function () {
-    it("should amount the items barcodes",function () {
-        let tags = [{barcode:'IEM0000000',count:1},{barcode:'IEM0000003',count:2},{barcode:'IEM0000003',count:2}];
-        let result = pos.amountBarcodes(tags);
+describe("amountBarcodes", function () {
+    it("should amount the items barcodes", function () {
+        let itemsTag = [{barcode: 'IEM0000000', count: 1}, {barcode: 'IEM0000003', count: 2}, {
+            barcode: 'IEM0000003',
+            count: 2
+        }];
+        let result = pos.amountBarcodes(itemsTag);
         let itemsCount = [
             {
-                barcode:'IEM0000000',
-                count:1
+                barcode: 'IEM0000000',
+                count: 1
             },
             {
-                barcode:'IEM0000003',
-                count:4
+                barcode: 'IEM0000003',
+                count: 4
             }
         ];
         expect(result).toEqual(itemsCount);
     })
 });
 
+describe("matchPromotions", function () {
+    it("match the item's promotion type", function () {
+        let itemsCount = [
+            {
+                "barcode": "ITEM000001",
+                "count": 1
+            },
+            {
+                "barcode": "ITEM000003",
+                "count": 2.5
+            },
+            {
+                "barcode": "ITEM000005",
+                "count": 3
+            }
+        ];
+        let allPromoteItems = load.loadPromotions();
+        let result = pos.matchPromotions(itemsCount, allPromoteItems);
+
+        expect(result).toEqual([
+            {
+                "barcode": "ITEM000001",
+                "count": 1,
+                "type": "BUY_TWO_GET_ONE_FREE"
+            },
+            {
+                "barcode": "ITEM000003",
+                "count": 2.5,
+                "type": "1"
+            },
+            {
+                "barcode": "ITEM000005",
+                "count": 3,
+                "type": "BUY_TWO_GET_ONE_FREE"
+            }
+        ]);
+    })
+});
 
 
 
--- src/app.js
+++ src/app.js
@@ -26,9 +26,28 @@ function amountBarcodes(itemsTag) {
     return itemsCount;
 }
 
+function matchPromotions(itemsCount, allPromoteItems) {
+    let itemsPromotionList = [];
+    for (let i = 0; i < itemsCount.length; i++) {
+        allPromoteItems.find(function (item) {
+            let type = "1";
+            let existItems = item.barcodes.find(function (barcode) {
+                return barcode === itemsCount[i].barcode;
+            });
+            if (existItems) {
+                type = item.type;
+            }
+            itemsPromotionList.push(Object.assign({}, itemsCount[i], {type: type}));
+        });
+    }
+
+    return itemsPromotionList;
+}
+
 module.exports = {
     separateTags: separateTags,
-    amountBarcodes: amountBarcodes
+    amountBarcodes: amountBarcodes,
+    matchPromotions: matchPromotions
 };
 
 
