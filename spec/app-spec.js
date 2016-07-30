3d08852958a7325a807765240ebc0c94294ffb50
Afaren <550047450@qq.com>
2016-07-30 11:00:56 +0800

    finish function mergeBarcodes

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -25,4 +25,30 @@ describe('formatTags', () => {
     let actual = core.formatTags(tags);
     expect(actual).toEqual(expected);
   })
+});
+
+
+describe('mergeBarcode', () => {
+  it('should return merged barcode amount list', () => {
+    let formattedTags = [{
+      barcode: 'ITEM000001', amount: 1
+    }, {
+      barcode: 'ITEM000001', amount: 1
+    }, {
+      barcode: 'ITEM000003', amount: 2
+    }, {
+      barcode: 'ITEM000003', amount: 2.5
+    }, {
+      barcode: 'ITEM000005', amount: 1
+    }];
+    let mergedBarcodes = [{
+      barcode: 'ITEM000001', amount: 2
+    }, {
+      barcode: 'ITEM000003', amount: 4.5
+    }, {
+      barcode: 'ITEM000005', amount: 1
+    }];
+    let actual = core.mergeTags(formattedTags);
+    expect(actual).toEqual(mergedBarcodes);
+  })
 })
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 
 function formatTags(tags) {
-  return tags.map( tag => {
+  return tags.map(tag => {
     let tagPart = tag.split('-');
     return {
       barcode: tagPart[0],
@@ -11,7 +11,20 @@ function formatTags(tags) {
   })
 }
 
+function mergeTags(formattedTags) {
+  return formattedTags.reduce((acc, cur) => {
+    let found = acc.find(i => i.barcode === cur.barcode);
+    if (found)
+      found.amount += cur.amount;
+    else
+      acc.push(cur);
+    return acc;
+  }, [])
+}
+
 
 module.exports = {
   formatTags,
+  mergeTags,
+
 };
