7de336709189f5974c18df76e8fcf03ffb183da1
ym <942470581@qq.com>
2016-07-30 11:58:25 +0800

    app.js

--- src/app.js
+++ src/app.js
@@ -65,9 +65,78 @@ function matchPromotionType(amountBarList,loadPromotions) {
 
 }
 
+function matchAllItems(promotionItems,loadAllItems) {
+    let cartItems=[];
+
+    for (let k=0;k<promotionItems.length;k++){
+        cartItems.push(promotionItems[k]);
+    }
+
+    for(let i=0;i<cartItems.length;i++){
+        for (let j=0;j<loadAllItems.length;j++){
+            if (cartItems[i].barcode === loadAllItems[j].barcode){
+                cartItems[i].name=loadAllItems[j].name;
+                cartItems[i].price=loadAllItems[j].price;
+                cartItems[i].unit=loadAllItems[j].unit;
+            }
+        }
+    }
+    return cartItems;
+}
+
+function calSavingList(cartItems) {
+    let savingList=[];
+
+    for (let i=0;i<cartItems.length;i++){
+        savingList.push(cartItems[i]);
+        savingList[i].saving=0;
+        if (cartItems[i].type==='BUY_TWO_GET_ONE_FREE'){
+            let price=parseFloat(savingList[i].price);
+            let amount=Math.floor(savingList[i].amount/3);
+            savingList[i].saving=parseFloat(price*amount);
+        }
+
+    }
+
+    return(savingList);
+}
+
+function calSubtotalList(savingList) {
+    let subtotalList=[];
+    for(let i=0;i<savingList.length;i++){
+        subtotalList.push(savingList[i]);
+        subtotalList[i].subtotal=subtotalList[i].amount*subtotalList[i].price-subtotalList[i].saving;
+    }
+
+    return subtotalList;
+}
+
+
+function calWholeSaving(subtotalList) {
+    let sumSaving=0;
+    Number(sumSaving);
+    for(let i=0;i<subtotalList.length;i++){
+        sumSaving+=Number(subtotalList[i].saving);
+    }
+
+    return sumSaving;
+}
+
+function calTotal(subtotalList) {
+    let totalNum=0;
+    Number(totalNum);
+    for(let i=0;i<subtotalList.length;i++){
+        totalNum+=Number(subtotalList[i].subtotal);
+    }
+
+    return totalNum;
+}
 
 
 module.exports={
     getCartItems:getCartItems,calAmountItems:calAmountItems,
-    matchPromotionType:matchPromotionType
+    matchPromotionType:matchPromotionType,
+    matchAllItems:matchAllItems,calSavingList:calSavingList,
+    calSubtotalList:calSubtotalList,calWholeSaving:calWholeSaving,
+    calTotal:calTotal
 }
