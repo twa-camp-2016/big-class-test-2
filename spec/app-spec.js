37991be26733b346e6dcae2144eab06b3c4988f6
bohekafeiting <1363896952@qq.com>
2016-07-29 23:49:30 -0400

    zhaosiyuan getitemsamount

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,6 +1,6 @@
 /*global describe,require*/
 const  links=require('../src/app');
-describe('getItemsAmount',function () {
+describe('getItems',function () {
     it('it should print items amount', function () {
         let inputs = ['ITEM000001', 'ITEM000001', 'ITEM000001', 'ITEM000001',
             'ITEM000001', 'ITEM000003-2', 'ITEM000005', 'ITEM000005', 'ITEM000005'];
@@ -13,6 +13,25 @@ describe('getItemsAmount',function () {
             {barcode: 'ITEM000005', amount: 1},
             {barcode: 'ITEM000005', amount: 1},
             {barcode: 'ITEM000005', amount: 1}]
+        let result=links.getItems(inputs);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe('getItemsAmount',function () {
+    it('it should print items amount', function () {
+        let inputs=[{barcode: 'ITEM000001', amount: 1},
+            {barcode: 'ITEM000001', amount: 1},
+            {barcode: 'ITEM000001', amount: 1},
+            {barcode: 'ITEM000001', amount: 1},
+            {barcode: 'ITEM000001', amount: 1},
+            {barcode: 'ITEM000003', amount: 2},
+            {barcode: 'ITEM000005', amount: 1},
+            {barcode: 'ITEM000005', amount: 1},
+            {barcode: 'ITEM000005', amount: 1}];
+        let expected=[{barcode: 'ITEM000001', amount: 5},
+            {barcode: 'ITEM000003', amount: 2},
+            {barcode: 'ITEM000005', amount: 3}];
         let result=links.getItemsAmount(inputs);
         expect(result).toEqual(expected);
     });
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 /*global module*/
 
-function getItemsAmount(tage) {
-    return tage.map(tag=> {
+function getItems(tags) {
+    return tags.map(tag=> {
         let temp = tag.split('-');
         return {
             barcode: temp[0],
@@ -10,4 +10,27 @@ function getItemsAmount(tage) {
     });
 }
 
-module.exports={getItemsAmount:getItemsAmount}
+function getItemsAmount(items) {
+   let itemsAmount=[];
+    for(let item of items){
+     let exit=itemsAmount.find(function (temp) {
+         return item.barcode===temp.barcode;
+        });
+     if(exit){
+         exit.itemsAmount+=item.itemsAmount;
+     }
+     else{
+       itemsAmount.push(Object.assign({},item));
+     }
+    }
+   return itemsAmount;
+}
+
+function getCartItems(itemsAmount) {
+
+}
+
+module.exports={getItems:getItems,
+               getItemsAmount:getItemsAmount,
+               getCartItems:getCartItems}
+
