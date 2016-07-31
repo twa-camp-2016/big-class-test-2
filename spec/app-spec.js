12c3d774d6f1fbdf2e9decc58e2f490d29a1de05
karida-bean <1816063167@qq.com>
2016-07-31 14:31:06 +0800

    pos

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,9 +1,240 @@
 const num = require("../src/app.js");
-describe('',function(){
-    it('',function(){
-        let input = [10001-1.5,10002,1003];
-        let expected = [{barcode:10001,amount:1.5},{barcode:10002,amount:1},{barcode:10003,amount:1}];
-        expect(num.getItemAmount(input)).toEqual(expected);
+describe('', function () {
+    it('', function () {
+        let input = ['10001-1.5', '10002', '10003'];
+        let expected = [{barcode: '10001', amount: 1.5}, {barcode: '10002', amount: 1}, {barcode: '10003', amount: 1}];
+        expect(num.getItemAmounts(input)).toEqual(expected);
 
+    })
+})
+describe("getAllItemAmounts", function () {
+    it("", function () {
+        let inputs = [{barcode: '10001', amount: 1.5}, {barcode: '10002', amount: 1}, {barcode: '10001', amount: 1}];
+        let outputs = num.getAllItemAmounts(inputs);
+        let expected = [{barcode: '10001', amount: 2.5}, {barcode: '10002', amount: 1}];
+        expect(outputs).toEqual(expected);
+    })
+})
+describe("get cart item ", function () {
+    it('', function () {
+        let inputs1 = [{barcode: "10001", amount: 2}];
+        let inputs2 = [{barcode: '10001', name: '苹果', price: 3.5, unit: '斤'}, {
+            barcode: '10002',
+            name: '啤酒',
+            price: 6,
+            unit: '瓶'
+        }];
+        let expected = [{barcode: '10001', name: "苹果", price: 3.5, unit: '斤', amount: 2}];
+        expect(num.getCartItems(inputs1, inputs2)).toEqual(expected);
+    })
+})
+describe("get promoted type", function () {
+    it("", function () {
+        let inputs1 = [
+            {
+                type: 'BUY_TWO_GET_ONE_FREE',
+                barcodes: [
+                    '10000',
+                    '10001',
+                    '10005'
+                ]
+            }
+        ];
+        let inputs2 = [{barcode: "10001", name: "苹果", price: 3.5, unit: "斤", amount: 2}, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3
+        }];
+        let expected = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 2,
+            type: "BUY_TWO_GET_ONE_FREE"
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none"
+        }];
+        expect(num.promotedType(inputs1, inputs2)).toEqual(expected);
+    });
+});
+
+describe("get promoted amount", function () {
+    it("", function () {
+        let inputs = [{barcode: "10001", name: "苹果", price: 3.5, unit: "斤", amount: 4, type: "BUY_TWO_GET_ONE_FREE"}, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none"
+        }];
+        let expected = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3
+        }];
+        expect(num.calculatePromotedAmount(inputs)).toEqual(expected);
+    })
+})
+describe("calculate promoted money", function () {
+    it("", function () {
+        let inputs = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3
+        }];
+        let expected = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3,
+            promotedMoney:10.5
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3,
+            promotedMoney:18
+        }];
+        expect(num.calculatePromotedMoney(inputs)).toEqual(expected);
+    });
+});
+
+describe("calculate subtotal",function(){
+    it("",function(){
+        let inputs = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3,
+            promotedMoney:10.5
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3,
+            promotedMoney:18
+        }];
+        let expected = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3,
+            promotedMoney:10.5,
+            subTotal:14
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3,
+            promotedMoney:18,
+            subTotal:18
+        }];
+        expect(num.calculateSubTotal(inputs)).toEqual(expected);
+    });
+});
+describe("calculate total",function(){
+    it("",function(){
+        let inputs = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3,
+            promotedMoney:10.5,
+            subTotal:14
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3,
+            promotedMoney:18,
+            subTotal:18
+        }];
+        let expected = 28.5;
+        expect(num.calculateTotal(inputs)).toEqual(expected);
+    });
+});
+describe("calculate promoted total money",function(){
+    it("",function(){
+        let inputs1 = [{
+            barcode: "10001",
+            name: "苹果",
+            price: 3.5,
+            unit: "斤",
+            amount: 4,
+            type: "BUY_TWO_GET_ONE_FREE",
+            promotedAmount: 3,
+            promotedMoney:10.5,
+            subTotal:14
+        }, {
+            barcode: "10002",
+            name: "啤酒",
+            price: 6,
+            unit: "瓶",
+            amount: 3,
+            type: "none",
+            promotedAmount: 3,
+            promotedMoney:18,
+            subTotal:18
+        }];
+        let inputs2 = 28.5;
+        let expected = 3.5;
+        expect(num.calculatePromotedTotalMoney(inputs1,inputs2)).toEqual(expected);
     })
 })
--- src/app.js
+++ src/app.js
@@ -21,69 +21,68 @@ function getAllItemAmount(itemAmount) {
     }, [])
 }
 
-function getCartItems(itemAllItems, allItems) {
-   let result = allItems.filter(function (all) {
-        itemAllItems.find(function (item) {
-            return item.barcode === all.barcode;
-        });
-    });
-    return result.map(function(res){
-        let thing = itemAllItems.find(function(item){
-            return item.barcode === res.barcode;
-        });
-        return res.amount = thing.amount;
+function getCartItems(itemAmounts, allItems) {
+    return itemAmounts.map(item =>{
+        return Object.assign({},allItems.find(all =>all.barcode === item.barcode),{amount:item.amount})
     })
 }
 
 function promotedType(promotions, cartItems) {
-    let result = promotions.filter(item)
-    {
-        return item.type = 'BUY_TWO_GET_ONE_FREE';
-    }
-    let arr = result.type;
-    return cartItems.map(function (item) {
-        let type = arr.find(function (a) {
-            return a === item.barcode;
-        })
-        if (type) {
-            item.type = 'BUY_TWO_GET_ONE_FREE';
-        } else {
-            item.type = 'none';
-        }
-    });
-}
-
-function calculatePromotedAmount(promotedTypes){
-    return promotedTypes.map(function(item){
-        if(item.type === "BUY_TWO_GET_ONE_FREE"){
-            item.promotedAmount = item.amount-item.amount%3
+    let promoteType = promotions.find(pro =>pro.hasOwnProperty('type')).barcodes;
+    return cartItems.map(function(item){
+       let result = promoteType.find(function(entry){
+          return entry === item.barcode;
+       });
+        if(result){
+            return Object.assign({},item,{type:"BUY_TWO_GET_ONE_FREE"})
         }else{
-            item.promotedAmount = item.amount;
+            return Object.assign({},item,{type:"none"})
+        }
+
+    })
+}
+function calculatePromotedAmount(promotedTypes){
+    return promotedTypes.map(item =>{
+        if(item.type === "BUY_TWO_GET_ONE_FREE"){
+            return Object.assign({},item,{promotedAmount:item.amount-parseInt(item.amount/3)})
+        }else{
+            return Object.assign({},item,{promotedAmount:item.amount});
         }
     });
 }
 
 function calculatePromotedMoney(proAmounts){
-    return proAmounts.map(function(item){
-        return item.promotedMoney = item.price * item.promotedAmount;
+    return proAmounts.map(item=>{
+        return Object.assign({},item,{promotedMoney:item.price * item.promotedAmount});
     });
 }
 
 function calculateSubTotal(proMoneys){
-    return proMoneys.map(function(item){
-        return item.subTotal = item.price * item.amount;
+    return proMoneys.map(item=>{
+        return Object.assign({},item,{subTotal:item.price * item.amount});
     });
 }
 
 function calculateTotal(subTotals){
-    return subTotals.reduce(function(cur,item){
-        return cur+item.promotedMoney;
-    });
+    return subTotals.reduce((cur,item)=>{
+        return cur + item.promotedMoney;
+    },0);
 }
 
 function calculatePromotedTotalMoney(subTotals,total){
-    let resullt = subTotals.reduce(function(cur,item){
-        return cur+item.subTotal;
-    });
+    let resullt = subTotals.reduce((cur,item)=>{
+        return cur + item.subTotal;
+    },0);
     return resullt - total;
+}
+module.exports = {
+    getItemAmounts:getItemAmount,
+    getAllItemAmounts:getAllItemAmount,
+    getCartItems:getCartItems,
+    promotedType:promotedType,
+    calculatePromotedAmount:calculatePromotedAmount,
+    calculatePromotedMoney:calculatePromotedMoney,
+    calculateSubTotal:calculateSubTotal,
+    calculateTotal:calculateTotal,
+    calculatePromotedTotalMoney:calculatePromotedTotalMoney
 }
