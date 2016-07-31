3842c51383c75886eca29f844333892047a7850e
bohekafeiting <1363896952@qq.com>
2016-07-31 03:32:45 -0400

    zhaosiyuan caculatesubtotal_spec

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -114,8 +114,8 @@ describe('calculateOriginalSubtotal', function () {
 });
 
 describe('getCartItemsPromotion()', function () {
-    it('it should print items amount', function () {
-         let inputs = [{
+    it('it should print items promotion', function () {
+        let inputs = [{
             barcode: 'ITEM000001',
             name: '雪碧',
             unit: '瓶',
@@ -137,14 +137,14 @@ describe('getCartItemsPromotion()', function () {
             amount: 3,
             originalSubtotal: 13.5
         }];
-        let expected=[{
+        let expected = [{
             barcode: 'ITEM000001',
             name: '雪碧',
             unit: '瓶',
             price: 3.00,
             amount: 5,
             originalSubtotal: 15,
-            type:'BUY_TWO_GET_ONE_FREE'
+            type: 'BUY_TWO_GET_ONE_FREE'
         }, {
             barcode: 'ITEM000003',
             name: '荔枝',
@@ -159,9 +159,67 @@ describe('getCartItemsPromotion()', function () {
             price: 4.50,
             amount: 3,
             originalSubtotal: 13.5,
-            type:'BUY_TWO_GET_ONE_FREE'
+            type: 'BUY_TWO_GET_ONE_FREE'
         }];
         let result = links.getCartItemsPromotion(inputs);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe('calculateSubtotal()()', function () {
+    it('it should print items subtotal with promotion', function () {
+        let inputs = [{
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00,
+            amount: 5,
+            originalSubtotal: 15,
+            type: 'BUY_TWO_GET_ONE_FREE'
+        }, {
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00,
+            amount: 2,
+            originalSubtotal: 30,
+        }, {
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.50,
+            amount: 3,
+            originalSubtotal: 13.5,
+            type: 'BUY_TWO_GET_ONE_FREE'
+        }];
+        let expected=[{
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00,
+            amount: 5,
+            originalSubtotal: 15,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            subtotal: 12
+        }, {
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00,
+            amount: 2,
+            originalSubtotal: 30,
+            subtotal: 30,
+        }, {
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.50,
+            amount: 3,
+            originalSubtotal: 13.5,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            subtotal: 9
+        }];
+        let result = links.calculateSubtotal(inputs);
         expect(result).toEqual(expected);
     });
 });
