3b3c69b83b79ee8020da93b8da16a2e504c0e2ce
congcong1993 <18829291885@163.com>
2016-07-31 12:38:41 +0800

    2

--- spec/app-spec.js
+++ spec/app-spec.js
@@ -1,263 +1,263 @@
 'use strict';
 const app = require("../src/app.js");
 const fixture = require("./fixture.js");
-// describe("test splitTag", function () {
-//     it("show splitTag", function () {
-//         let tags = ['ITEM000001', 'ITEM000003-2','ITEM000002-2.5'];
-//         let result = app.splitTag(tags);
-//         expect(result).toEqual([
-//             {
-//                 "barcode": "ITEM000001",
-//                 "count": 1
-//             },
-//             {
-//                 "barcode": "ITEM000003",
-//                 "count": 2
-//             },{
-//                 "barcode": "ITEM000002",
-//                 "count": 2.5
-//             }
-//         ]);
-//     })
-// });
+describe("test splitTag", function () {
+    it("show splitTag", function () {
+        let tags = ['ITEM000001', 'ITEM000003-2','ITEM000002-2.5'];
+        let result = app.splitTag(tags);
+        expect(result).toEqual([
+            {
+                "barcode": "ITEM000001",
+                "count": 1
+            },
+            {
+                "barcode": "ITEM000003",
+                "count": 2
+            },{
+                "barcode": "ITEM000002",
+                "count": 2.5
+            }
+        ]);
+    })
+});
 
-// describe("test getTagCount", function () {
-//     it("show getTagCount", function () {
-//         let splitTag = [{
-//             barcode: 'ITEM000002',
-//             count: 1
-//         }, {
-//             barcode: 'ITEM000001',
-//             count: 2
-//         }, {
-//             barcode: 'ITEM000001',
-//             count: 3
-//         }];
-//
-//         let result = app.getTagCount(splitTag);
-//
-//         expect(result).toEqual([
-//             {
-//                 barcode: 'ITEM000002',
-//                 count: 1
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 count: 5
-//             }
-//         ])
-//     })
-// });
+describe("test getTagCount", function () {
+    it("show getTagCount", function () {
+        let splitTag = [{
+            barcode: 'ITEM000002',
+            count: 1
+        }, {
+            barcode: 'ITEM000001',
+            count: 2
+        }, {
+            barcode: 'ITEM000001',
+            count: 3
+        }];
 
-// describe("test matchPromotion", function () {
-//     it("show matchPromotion", function () {
-//         let allTagCount = [
-//             {
-//                 barcode: 'ITEM000005',
-//                 count: 1
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 count: 2
-//             }
-//         ];
-//         let promotion = fixture.loadPromotions();
-//         let result = app.matchPromotion(allTagCount, promotion);
-//         expect(result).toEqual([
-//             {
-//                 barcode: 'ITEM000005',
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 count: 1
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 count: 2
-//             }
-//         ]);
-//     })
-// })
+        let result = app.getTagCount(splitTag);
 
-// describe('test matchItems', function () {
-//     it('show cartItemsCount', function () {
-//         let itemsPromotion = [
-//             {
-//                 barcode: 'ITEM000002',
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 count: 1
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 count: 2
-//             }
-//         ];
-//         let allItems=fixture.loadAllItems();
-//         let result = app.matchItems(itemsPromotion, allItems);
-//         expect(result).toEqual([
-//             {
-//                 barcode: 'ITEM000002',
-//                 name: '苹果',
-//                 unit: '斤',
-//                 price: 5.50,
-//                 count:1,
-//                 type:'BUY_TWO_GET_ONE_FREE'
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 name: '雪碧',
-//                 unit: '瓶',
-//                 price: 3.00,
-//                 count: 2,
-//                 type: 'BUY_TWO_GET_ONE_FREE'
-//             }
-//         ]);
-//     })
-// })
+        expect(result).toEqual([
+            {
+                barcode: 'ITEM000002',
+                count: 1
+            }, {
+                barcode: 'ITEM000001',
+                count: 5
+            }
+        ])
+    })
+});
 
-// describe('test getDiscountSubtotal', function () {
-//     it('show discountSubtotal', function () {
-//         let cartItemsCount = [
-//             {
-//                 barcode: 'ITEM000002',
-//                 name: '苹果',
-//                 unit: '斤',
-//                 price: 5.50,
-//                 count:1,
-//                 type:'BUY_TWO_GET_ONE_FREE'
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 name: '雪碧',
-//                 unit: '瓶',
-//                 price: 3.00,
-//                 count: 6,
-//                 type: 'BUY_TWO_GET_ONE_FREE'
-//             }];
-//         let promotion = fixture.loadPromotions();
-//         let result=app.getDiscountSubtotal(cartItemsCount,promotion);
-//         expect(result).toEqual([
-//             {
-//                 barcode: 'ITEM000002',
-//                 name: '苹果',
-//                 unit: '斤',
-//                 price: 5.50,
-//                 count: 1,
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 discountSubtotalSum:5.50
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 name: '雪碧',
-//                 unit: '瓶',
-//                 price: 3.00,
-//                 count: 6,
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 discountSubtotalSum:12.00
-//             }
-//         ]);
-//
-//     })
-// })
-// describe('test getSubtotal',function () {
-//     it('show subtotal',function () {
-//         let cartItemsCount=[
-//             {
-//                 barcode: 'ITEM000002',
-//                 name: '苹果',
-//                 unit: '斤',
-//                 price: 5.50,
-//                 count:1,
-//                 type:'BUY_TWO_GET_ONE_FREE'
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 name: '雪碧',
-//                 unit: '瓶',
-//                 price: 3.00,
-//                 count: 6,
-//                 type: 'BUY_TWO_GET_ONE_FREE'
-//             }
-//         ];
-//         let result=app.getSubtotal(cartItemsCount);
-//         expect(result).toEqual([
-//             {
-//                 barcode: 'ITEM000002',
-//                 name: '苹果',
-//                 unit: '斤',
-//                 price: 5.50,
-//                 count:1,
-//                 type:'BUY_TWO_GET_ONE_FREE',
-//                 noDiscountSubtotal:5.50
-//             }, {
-//                 barcode: 'ITEM000001',
-//                 name: '雪碧',
-//                 unit: '瓶',
-//                 price: 3.00,
-//                 count: 6,
-//                 type: 'BUY_TWO_GET_ONE_FREE',
-//                 noDiscountSubtotal:18
-//             }
-//         ]);
-//     })
-// })
+describe("test matchPromotion", function () {
+    it("show matchPromotion", function () {
+        let allTagCount = [
+            {
+                barcode: 'ITEM000005',
+                count: 1
+            }, {
+                barcode: 'ITEM000001',
+                count: 2
+            }
+        ];
+        let promotion = fixture.loadPromotions();
+        let result = app.matchPromotion(allTagCount, promotion);
+        expect(result).toEqual([
+            {
+                barcode: 'ITEM000005',
+                type: 'BUY_TWO_GET_ONE_FREE',
+                count: 1
+            }, {
+                barcode: 'ITEM000001',
+                type: 'BUY_TWO_GET_ONE_FREE',
+                count: 2
+            }
+        ]);
+    })
+})
 
-// describe('test getSaveTotal',function () {
-//     it('show saveTotal',function () {
-//         let discountSubtotal=[ {
-//             barcode: 'ITEM000002',
-//             name: '苹果',
-//             unit: '斤',
-//             price: 5.50,
-//             count: 1,
-//             type: 'BUY_TWO_GET_ONE_FREE',
-//             discountSubtotalSum:5.50
-//         }, {
-//             barcode: 'ITEM000001',
-//             name: '雪碧',
-//             unit: '瓶',
-//             price: 3.00,
-//             count: 6,
-//             type: 'BUY_TWO_GET_ONE_FREE',
-//             discountSubtotalSum:12.00
-//         }];
-//         let subtotal=[ {
-//             barcode: 'ITEM000002',
-//             name: '苹果',
-//             unit: '斤',
-//             price: 5.50,
-//             count:1,
-//             type:'BUY_TWO_GET_ONE_FREE',
-//             noDiscountSubtotal:5.50
-//         }, {
-//             barcode: 'ITEM000001',
-//             name: '雪碧',
-//             unit: '瓶',
-//             price: 3.00,
-//             count: 6,
-//             type: 'BUY_TWO_GET_ONE_FREE',
-//             noDiscountSubtotal:18
-//         }];
-//         let result=app.getSaveTotal(discountSubtotal,subtotal);
-//         expect(result).toEqual(6);
-//     })
-// })
+describe('test matchItems', function () {
+    it('show cartItemsCount', function () {
+        let itemsPromotion = [
+            {
+                barcode: 'ITEM000002',
+                type: 'BUY_TWO_GET_ONE_FREE',
+                count: 1
+            }, {
+                barcode: 'ITEM000001',
+                type: 'BUY_TWO_GET_ONE_FREE',
+                count: 2
+            }
+        ];
+        let allItems=fixture.loadAllItems();
+        let result = app.matchItems(itemsPromotion, allItems);
+        expect(result).toEqual([
+            {
+                barcode: 'ITEM000002',
+                name: '苹果',
+                unit: '斤',
+                price: 5.50,
+                count:1,
+                type:'BUY_TWO_GET_ONE_FREE'
+            }, {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 2,
+                type: 'BUY_TWO_GET_ONE_FREE'
+            }
+        ]);
+    })
+})
 
-// describe('test getTotal',function () {
-//     it('show total',function () {
-//         let discountSubtotal=[{
-//             barcode: 'ITEM000002',
-//             name: '苹果',
-//             unit: '斤',
-//             price: 5.50,
-//             count: 1,
-//             type: 'BUY_TWO_GET_ONE_FREE',
-//             discountSubtotalSum:5.50
-//         }, {
-//             barcode: 'ITEM000001',
-//             name: '雪碧',
-//             unit: '瓶',
-//             price: 3.00,
-//             count: 6,
-//             type: 'BUY_TWO_GET_ONE_FREE',
-//             discountSubtotalSum:12.00
-//         }];
-//         let result=app.getTotal(discountSubtotal);
-//         expect(result).toEqual(17.5);
-//     })
-// })
+describe('test getDiscountSubtotal', function () {
+    it('show discountSubtotal', function () {
+        let cartItemsCount = [
+            {
+                barcode: 'ITEM000002',
+                name: '苹果',
+                unit: '斤',
+                price: 5.50,
+                count:1,
+                type:'BUY_TWO_GET_ONE_FREE'
+            }, {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 6,
+                type: 'BUY_TWO_GET_ONE_FREE'
+            }];
+        let promotion = fixture.loadPromotions();
+        let result=app.getDiscountSubtotal(cartItemsCount,promotion);
+        expect(result).toEqual([
+            {
+                barcode: 'ITEM000002',
+                name: '苹果',
+                unit: '斤',
+                price: 5.50,
+                count: 1,
+                type: 'BUY_TWO_GET_ONE_FREE',
+                discountSubtotalSum:5.50
+            }, {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 6,
+                type: 'BUY_TWO_GET_ONE_FREE',
+                discountSubtotalSum:12.00
+            }
+        ]);
+
+    })
+})
+describe('test getSubtotal',function () {
+    it('show subtotal',function () {
+        let cartItemsCount=[
+            {
+                barcode: 'ITEM000002',
+                name: '苹果',
+                unit: '斤',
+                price: 5.50,
+                count:1,
+                type:'BUY_TWO_GET_ONE_FREE'
+            }, {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 6,
+                type: 'BUY_TWO_GET_ONE_FREE'
+            }
+        ];
+        let result=app.getSubtotal(cartItemsCount);
+        expect(result).toEqual([
+            {
+                barcode: 'ITEM000002',
+                name: '苹果',
+                unit: '斤',
+                price: 5.50,
+                count:1,
+                type:'BUY_TWO_GET_ONE_FREE',
+                noDiscountSubtotal:5.50
+            }, {
+                barcode: 'ITEM000001',
+                name: '雪碧',
+                unit: '瓶',
+                price: 3.00,
+                count: 6,
+                type: 'BUY_TWO_GET_ONE_FREE',
+                noDiscountSubtotal:18
+            }
+        ]);
+    })
+})
+
+describe('test getSaveTotal',function () {
+    it('show saveTotal',function () {
+        let discountSubtotal=[ {
+            barcode: 'ITEM000002',
+            name: '苹果',
+            unit: '斤',
+            price: 5.50,
+            count: 1,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            discountSubtotalSum:5.50
+        }, {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00,
+            count: 6,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            discountSubtotalSum:12.00
+        }];
+        let subtotal=[ {
+            barcode: 'ITEM000002',
+            name: '苹果',
+            unit: '斤',
+            price: 5.50,
+            count:1,
+            type:'BUY_TWO_GET_ONE_FREE',
+            noDiscountSubtotal:5.50
+        }, {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00,
+            count: 6,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            noDiscountSubtotal:18
+        }];
+        let result=app.getSaveTotal(discountSubtotal,subtotal);
+        expect(result).toEqual(6);
+    })
+})
+
+describe('test getTotal',function () {
+    it('show total',function () {
+        let discountSubtotal=[{
+            barcode: 'ITEM000002',
+            name: '苹果',
+            unit: '斤',
+            price: 5.50,
+            count: 1,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            discountSubtotalSum:5.50
+        }, {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00,
+            count: 6,
+            type: 'BUY_TWO_GET_ONE_FREE',
+            discountSubtotalSum:12.00
+        }];
+        let result=app.getTotal(discountSubtotal);
+        expect(result).toEqual(17.5);
+    })
+})
 
 describe("test printReceipt", function () {
     it("show receiptText", function () {
