f164f0c21ab20b9f24562601a2178dada5d5059e
liuru123 <liuru@1457816105qq.com>
2016-08-01 19:04:49 +0800

    modify

--- src/app.js
+++ src/app.js
@@ -1,121 +1,104 @@
 'use strict';
-const fix=require('../spec/fixture');
 
-
-
+const fix = require('../spec/fixture');
 
 function dividedItems(inputs) {
-    let result=[];
+    let result = [];
     inputs.forEach(function (input) {
-        let temp=input.split('-');
-        result.push({barcode:temp[0],count:parseFloat(temp[1]) || 1});
+        let temp = input.split('-');
+        result.push({barcode: temp[0], count: parseFloat(temp[1]) || 1});
     });
     return result;
 }
-
 
 function computeItems(inputDivide) {
-    let result=[];
+    let result = [];
     inputDivide.forEach(function (input) {
-
         let exist = result.find(function (item) {
-            return item.barcode ===input.barcode;
+            return item.barcode === input.barcode;
         });
-        if(exist)
-        {
-            exist.amount+=input.count;
+        if (exist) {
+            exist.amount += input.count;
         }
-        else
-        {
-            result.push({barcode:input.barcode,amount:1});
+        else {
+            result.push({barcode: input.barcode, amount: 1});
         }
     });
     return result;
 }
 
-
-
-function generateItems(inputDivideCompute,items) {
-    let result=[];
+function generateItems(inputDivideCompute, items) {
+    let result = [];
     inputDivideCompute.forEach(function (input) {
         let exist = items.find(function (item) {
-            return input.barcode===item.barcode;
+            return input.barcode === item.barcode;
         });
-        if(exist) {
-            result.push({barcode:exist.barcode,
-                name:exist.name,
-                unit:exist.unit,
-                price:exist.price,
-                amount:input.amount});
+        if (exist) {
+            result.push(Object.assign({}, exist, {
+                amount: input.amount
+            }));
         }
     });
     return result;
 }
 
-function computeSubtotal(itemsMatch,promotionArray) {
-    let result=[];
+function computeSubtotal(itemsMatch, promotionArray) {
+    let result = [];
     itemsMatch.forEach(function (promotion) {
-
-   let exist = promotionArray[0].barcodes.find(function (item) {
-        return item===promotion.barcode;
+        let exist = promotionArray[0].barcodes.find(function (item) {
+            return item === promotion.barcode;
+        });
+        result.push(Object.assign({}, promotion,
+            {
+                subtotal: (promotion.amount - parseInt(promotion.amount / 3)) * promotion.price,
+                save: parseInt(promotion.amount / 3) * promotion.price
+            }));
     });
-      if(exist)
-      {
-          result.push({barcode:exist,
-              name:promotion.name,
-              unit:promotion.unit,
-              price:promotion.price,
-              amount:promotion.amount,subtotal:(promotion.amount-parseInt(promotion.amount/3))*promotion.price,save:parseInt(promotion.amount/3)*promotion.price});
-      }
-      else
-      {
-          result.push({barcode:promotion.barcode,
-              name:promotion.name,
-              unit:promotion.unit,
-              price:promotion.price,
-              amount:promotion.amount,subtotal:promotion.price*promotion.amount,save:0});
-      }
-})
     return result;
 }
 
 function computeTotal(itemsSubtotal) {
-    let result=[];
-    let sum=0;
-    let save=0;
+    let result = [];
+    let sum = 0;
+    let save = 0;
     itemsSubtotal.forEach(function (item) {
-        sum+=item.subtotal;
-        save+=item.save;
+        sum += item.subtotal;
+        save += item.save;
     })
     result.push(sum);
     result.push(save);
     return result;
 }
 
-function print (itemsSubtotal,total) {
-    let result=`***<没钱赚商店>收据***`;
+function print(itemsSubtotal, total) {
+    let result = `***<没钱赚商店>收据***`;
     itemsSubtotal.forEach(function (item) {
-        result+=`\n名称：`+item.name+`，`+`数量：`+item.amount+item.unit+`，`+`单价：`+item.price.toFixed(2)+`(元)`+`，`+`小计：`+item.subtotal.toFixed(2)+`(元)`;
+        result += `\n名称：` + item.name + `，` + `数量：` + item.amount + item.unit + `，` + `单价：` + item.price.toFixed(2) + `(元)` + `，` + `小计：` + item.subtotal.toFixed(2) + `(元)`;
     })
-    result+=`\n----------------------`;
-    result+=`\n总计：`+total[0].toFixed(2)+`(元)`;
-    result+=`\n节省：`+total[1].toFixed(2)+`(元)`;
-    result+=`\n**********************`;
+    result += `\n----------------------`;
+    result += `\n总计：` + total[0].toFixed(2) + `(元)`;
+    result += `\n节省：` + total[1].toFixed(2) + `(元)`;
+    result += `\n**********************`;
     return result;
 }
-
 
 function printReceipt(inputs) {
-    let items=fix.loadAllItems();
-    let promotionsArray=fix.loadPromotions();
-    let inputDivide=dividedItems(inputs);
-    let inputDivideCompute=computeItems(inputDivide);
-    let itemsMatch=generateItems(inputDivideCompute,items);
-    let itemsSubtotal=computeSubtotal(itemsMatch,promotionsArray);
-    let total=computeTotal(itemsSubtotal);
-    let result=print(itemsSubtotal,total);
-
+    let items = fix.loadAllItems();
+    let promotionsArray = fix.loadPromotions();
+    let inputDivide = dividedItems(inputs);
+    let inputDivideCompute = computeItems(inputDivide);
+    let itemsMatch = generateItems(inputDivideCompute, items);
+    let itemsSubtotal = computeSubtotal(itemsMatch, promotionsArray);
+    let total = computeTotal(itemsSubtotal);
+    let result = print(itemsSubtotal, total);
     return result;
-
 }
-module.exports={dividedItems:dividedItems,computeItems:computeItems,generateItems:generateItems,computeSubtotal:computeSubtotal,computeTotal:computeTotal,print:print,printReceipt:printReceipt};
+module.exports = {
+    dividedItems: dividedItems,
+    computeItems: computeItems,
+    generateItems: generateItems,
+    computeSubtotal: computeSubtotal,
+    computeTotal: computeTotal,
+    print: print,
+    printReceipt: printReceipt
+};
